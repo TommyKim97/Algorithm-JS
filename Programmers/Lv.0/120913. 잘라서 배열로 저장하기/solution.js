c98011584ff8b57dfac820edf495a197772fbd2f
TommyKim97 <guana97@naver.com>
2023-03-18 10:24:16 +0900

    120913. 잘라서 배열로 저장하기

---
+++ Programmers/Lv.0/120913. 잘라서 배열로 저장하기/solution.js
@@ -0,0 +1,9 @@
+function solution(my_str, n) {
+  let strArr = my_str.split("");
+  let answer = [];
+
+  while (strArr.length > 0) {
+    answer.push(strArr.splice(0, n).join(""));
+  }
+  return answer;
+}
