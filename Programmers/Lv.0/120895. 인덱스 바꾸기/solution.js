03cc7ec565dd03f3643a9cc43c4021a2d46b7956
TommyKim97 <guana97@naver.com>
2023-05-08 09:00:23 +0900

    다른 풀이 추가

--- Programmers/Lv.0/120895. 인덱스 바꾸기/solution.js
+++ Programmers/Lv.0/120895. 인덱스 바꾸기/solution.js
@@ -4,3 +4,16 @@ const solution = (my_string, num1, num2) => {
   result.splice(num2, 1, my_string[num1]);
   return result.join("");
 };
+
+/*
+다른 풀이 
+
+* 구조분해 할당 이용
+
+const solution (my_string, num1, num2) {
+    my_string = my_string.split("");
+    [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+    return my_string.join("");
+}
+
+*/
